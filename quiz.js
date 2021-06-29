2c5e69132eb156c86b728e56129b988dcbcb00b9
Nikita <naberdyev@gmail.com>
2021-06-30 01:45:16 +0400

    Sepatate constants and sctript

--- quiz.js
+++ quiz.js
@@ -1,18 +1,22 @@
 function init() {
     const quizEl = document.getElementById('quiz')
-    const submitButton = document.getElementsByClassName('submit-button')
-    const answerAlert = document.getElementsByClassName('alert')
+
 
 
     let quizStr = ''
     //loop through the array of question
     questions.forEach((question, qIndex) => {
         let answerStr = ''
+        const answerOrder = randomRange(question.a.length)
         question.a.forEach((answer, aIndex) => {
             answerStr += `
-            <li>
+            <li style="order:${answerOrder[aIndex]}">
                 <lable>
-                    <input type="radio" name="question-${qIndex}">
+                    <input 
+                        type="radio" 
+                        name="question-${qIndex}"
+                        data-correct="${question.correct === aIndex}"
+                    >
                     ${answer}
                 </lable>
             </li>
@@ -22,34 +26,56 @@ function init() {
         quizStr +=`
         <form> 
             <h1>${question.q}</h1>
-            <div class="alert">
-                
-            </div>
-            <ul>
+            <div class="alert">-</div>
+            <ul style="display: flex; flex-direction:column">
                 ${answerStr}
             </ul>
             <lable>
-                <input class="submit-button" value="Submit" type="button"> 
+                <button type="submit">Submit</button> 
             </lable>
         </form>
         `
     })
     quizEl.innerHTML = quizStr;
 
-        // for each question make some html
-        // form
-        // h1
-        // div.alert
-        // ul
-        // li
-        // ...
-        //button submit
+    quizEl.addEventListener('submit', function (e) {
+        e.preventDefault()
+        const selectedInput = e.target.querySelector('input[type=radio]:checked')
+        const alert = e.target.querySelector('div.alert')
+        console.log(selectedInput);
+        if (selectedInput === null) {
+            alert.innerHTML = 'You need to select an answer'
+        } else if (selectedInput.dataset.correct === "true") {
+            alert.innerHTML = "That's correct!"
+        } else {
+            alert.innerHTML = 'Oops! Try again!'
+        }
+    })
 }
 
-
-function checkAnswer(params) {
-    
-}
 
 init()
 
+
+
+function random(n) {
+    const r = Math.random() * n
+    return Math.floor(r)
+}
+
+function randomRange(x) {
+    const arr = []
+    for (let i = 0; i < x; i++) {
+        arr.push(i)
+    }
+
+    const randomArr = []
+    while(arr.length > 0) {
+        randomIndex = random(arr.length)
+        const randomNumber = arr[randomIndex]
+        randomArr.push(randomNumber)
+        arr.splice(randomIndex, 1)
+    }
+
+    return randomArr
+}
