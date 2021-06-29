0d8718263f4db08d70834564798c1ced0d7ebaf3
Nikita <naberdyev@gmail.com>
2021-06-29 23:58:27 +0400

    Sepatate constants and sctript

--- quiz.js
+++ quiz.js
@@ -1,71 +1,7 @@
-const questions = [
-    {
-        q: 'Which queen of England was the wife of both the last Lancastrian heir to the throne and the last Yorkist king?',
-        a: [
-            'Elizabeth of York',
-            'Anne Neville',
-            'Elizabeth Woodville'
-        ],
-        correct: 1
-    },
-    {
-        q: 'One Byzantine bride who was married into a western European royal family was criticised for her ‘fancy ways’. But what was her offensively fanciful act?',
-        a: [
-            'She would never take off her crown',
-            'She ate with a fork',
-            'She had to be carried everywhere in a litter'
-        ],
-        correct: 1
-    },
-    {
-        q: 'Which king of England was known as the ‘Hammer of the Scots’?',
-        a: [
-            'Edward I',
-            'Richard II',
-            'Edward II'
-        ],
-        correct: 0
-    },
-    {
-        q: 'Which of these ‘three things’ might be expected of you as a medieval queen?',
-        a: [
-            'Being pious, pretty and a peacemaker',
-            'Being wealthy, wise and worldly',
-            'Being rich, respectful and religious'
-        ],
-        correct: 0
-    },
-    {
-        q: 'Which royal marriage was rumoured to be the result of witchcraft?',
-        a: [
-            'Henry VI and Margaret of Anjou',
-            'Henry VII and Elizabeth of York',
-            'Edward IV and Elizabeth Woodville'
-        ],
-        correct: 2
-    },
-    {
-        q: 'Which king of England is thought to have spent fewer than six months of his reign on English soil, and may not even have spoken English?',
-        a: [
-            'Edward III',
-            'Henry II',
-            'Richard I'
-        ],
-        correct: 2
-    },
-    {
-        q: 'During the Second Barons’ War against Henry III, the sheriff of Essex plotted to attack London using… what?',
-        a: [
-            'Cockerels with firebombs attached to their feet',
-            'Catapults firing cow carcasses',
-            'Cats infected with the plague'
-        ],
-        correct: 0
-    }
-]
-
 function init() {
     const quizEl = document.getElementById('quiz')
+    const submitButton = document.getElementsByClassName('submit-button')
+    const answerAlert = document.getElementsByClassName('alert')
 
 
     let quizStr = ''
@@ -86,9 +22,15 @@ function init() {
         quizStr +=`
         <form> 
             <h1>${question.q}</h1>
+            <div class="alert">
+                
+            </div>
             <ul>
                 ${answerStr}
             </ul>
+            <lable>
+                <input class="submit-button" value="Submit" type="button"> 
+            </lable>
         </form>
         `
     })
@@ -103,4 +45,11 @@ function init() {
         // ...
         //button submit
 }
+
+
+function checkAnswer(params) {
+    
+}
+
 init()
+
